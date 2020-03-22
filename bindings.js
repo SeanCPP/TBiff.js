b8b7b4a27150bf7e05ffb49469cdde828066b57b
Sean Franklin <sean.patrick516@gmail.com>
2020-03-22 18:37:01 -0400

    added github link

--- bindings.js
+++ bindings.js
@@ -2,6 +2,7 @@
 // Template Bindings Framework
 // File: bindings.js
 // License: GNU GPL v3
+// https://github.com/SeanCPP/TBiff.js
 
 /**This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
